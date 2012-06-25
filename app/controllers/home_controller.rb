a7770cf9261d213332985057970d5741b9c88b6f
Etienne de Bruin <etdebruin@gmail.com>
2012-06-24 20:25:29 -0700

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def mission
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdcto::Application.config.secret_token = '4d4cc95413d18ad9161f1e25b63d6f93d364f83b88aaaa446203760f531a77794c14a3209bd607151405e24e6d1bf985199341156b98c2ddd8498d18ab903bc1'
